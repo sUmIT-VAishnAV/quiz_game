9c215ba5d2a98840f006828072440fa533830be9
Sumit_Vaishnav26 <113088444+sUmIT-VAishnAV@users.noreply.github.com>
2025-03-06 10:15:51 +0530

    added new result screen

---
+++ lib/data/questions.dart
@@ -0,0 +1,94 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What does CPU stand for?',
+    [
+      'Central Processing Unit',
+      'Central Process Unit',
+      'Computer Processing Unit',
+      'Core Processing Utility',
+    ],
+  ),
+  QuizQuestion(
+    'Which is the largest ocean on Earth?',
+    [
+      'Pacific  Ocean',
+      'Atlantic Ocean',
+      'Indian Ocean',
+      'Arctic  Ocean',
+    ],
+  ),
+  QuizQuestion(
+    'How many continents are there in the world?',
+    [
+      '7',
+      '5',
+      '8',
+      '6',
+    ],
+  ),
+  QuizQuestion(
+    'What is the powerhouse of the cell?',
+    [
+      'Mitochondria',
+      'Nucleus',
+      'Ribosome',
+      'Endoplasmic reticulum',
+    ],
+  ),
+  QuizQuestion(
+    'Which is the longest river in the world?',
+    [
+      'Nile River',
+      'Amazon River',
+      'Mississippi River',
+      'Yangtze River',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the founder of Microsoft?',
+    [
+      'Bill Gates ',
+      'Elon Musk',
+      'Mark Zuckerberg',
+      'Steve Jobs',
+    ],
+  ),
+  QuizQuestion(
+    'What is the most used programming language in the world?',
+    [
+      'JavaScript',
+      'Java',
+      'Python',
+      'C++',
+    ],
+  ),
+  QuizQuestion(
+    'Which company developed Android OS?',
+    [
+      'Google',
+      'Samsung',
+      'Microsoft',
+      'Apple',
+    ],
+  ),
+  QuizQuestion(
+    'Which planet is closest to the Sun?',
+    [
+      'Mercury ',
+      'Venus',
+      'Jupiter',
+      'Mars',
+    ],
+  ),
+  QuizQuestion(
+    'What is the fear of heights called?',
+    [
+      'Acrophobia',
+      'Claustrophobia',
+      'Agoraphobia',
+      'Arachnophobia',
+    ],
+  ),
+];
---
+++ lib/quiz.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions.dart';
+import 'package:quiz_app/results_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  switchScreen() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+    if (activeScreen == 'questions-screen') {
+      screenWidget = Questions(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
+    }
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromARGB(255, 207, 68, 204),
+                Color.fromARGB(255, 71, 175, 223),
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            ),
+            child: screenWidget),
+      ),
+    );
+  }
+}
